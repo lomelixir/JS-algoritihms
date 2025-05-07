731ba3300aa64cc50130d07e02ee5c738c4ed337
LOMELINUX <kevin.lome@hotmail.com>
2025-05-07 15:49:34 -0600

    Add new algorithms

--- algo2.js
+++ algo2.js
@@ -1,2 +1,132 @@
 console.log("HELLO WORLD ALGO2");
 
+//1.- Leer TRES números y descubrir si UNO es la suma de los otros DOS.
+function suma(a, b, c) {
+    if (a + b == c) {
+        console.log(`Se cumple el ejercicio. ${c} es igual a ${a} + ${b}`);
+    } else if (a + c == b) {
+        console.log(`Se cumple el ejercicio. ${b} es igual a ${a} + ${c}`);
+    } else if (b + c == a) {
+        console.log(`Se cumple el ejercicio. ${a} es igual a ${b} + ${c}`);
+    } else {
+        console.log("No se cumple el ejercicio.");
+    }
+}
+// suma(45, 6, 35); //CORRECTO
+
+
+
+//2.- Números primos iguales o menores que N.
+function primo(n) {
+    for (let i = 2; i <= n; i++) {
+        if (i % i === 0)  {
+            console.log(i);
+            console.log("es primo");
+        } else {
+            console.log(i);
+            console.log("no es primo");
+        }
+    }
+    for (let i = 2; i <= n; i++) {
+        if (i % 1 == 0) {
+            console.log("es primo 2");
+        } else {
+            console.log("es primo 2");
+        }
+    }
+    let dividendo = 3;
+    for (let i = 2; i <= n; i++) {
+        dividendo++;
+    }
+}
+// primo(3); //AÚN NO ME SALE, TENGO IDEAS.
+
+
+
+//3.- Calcular la superficie de un triángulo 
+function superficie(base, altura) {
+    let basePorAltura;
+    let superficieTriangulo;
+    basePorAltura = base * altura;
+    superficieTriangulo = basePorAltura / 2;
+    console.log(`La superficie del triángulo es ${superficieTriangulo}`);
+
+}
+// superficie(10, 5); //CORRECTO.
+
+
+
+//4.- Calcular la longitud de la circunferencia y el área de un círculo de radio dado.
+function circuloAreaCircuferencia(radio) {
+    const pi = 3.1416;
+    let circunferencia = 2 * pi * radio;
+    let area = pi * radio**2;
+    console.log(`El valor de la circunferencia es ${circunferencia} y el valor del área es ${area}.`);
+}
+// circuloAreaCircuferencia(20); //CORRECTO    
+
+
+
+//5.- Palíndromo //PENDIENTE
+// function palindromo(palabra) {
+//    for (let i = 0; i < palabra.length; i++) {
+//         let letra = palabra[i];
+//         for (let j = )
+//    }
+// }
+// palindromo("hola");//PENDIENTE 
+
+
+
+//6.- Calcular promedio 
+const precios = [100, 200, 300, 400, 500];
+
+function promedio(array) {
+    if (array.length == 0) {
+        console.log("No se puede obtener el promedio.")
+    }
+    let reducido = array.reduce((acc, el) => {
+        return acc + el;
+    }, 0);
+    let resultado = reducido / array.length;
+    console.log(`El precio promedio es de ${resultado}`);
+}
+
+// promedio(precios); //CORRECTO
+
+
+
+//7.- Invertir Array
+const numeros = [1, 2, 3, 4, 5];
+
+function invertirArray(array) {
+    let newArray = []; //Bien 
+    for (let i = array.length - 1; i >= 0; i--) { //Me faltó agregar -1 a i. La condición estaba mal.
+        newArray.push(array[i]);//No necesito asignar esto a nueva variable. 
+    }
+     console.log(newArray); //si pongo return no aparece
+}
+// invertirArray(numeros);// CORRECTO
+
+
+
+//8.- Combinar y ordenar arrays.
+const numeros1 = [10, 5, 8];
+const numeros2 = [2, 7, 3];
+
+function combinarOrdenar(array1, array2) {
+    let combinado = array1.concat(array2);
+    console.log(combinado);
+    let ordenado = combinado.sort();
+    console.log(ordenado);
+}
+// combinarOrdenar(numeros1, numeros2); //NO ME SALIÓ
+
+
+function combinarOrdenar2(array1, array2) {
+    let ordenado1 = array1.sort();
+    let ordenado2 = array2.sort();
+    let combinado = array1.concat(array2);
+    console.log(combinado);
+}
+// combinarOrdenar2(numeros1, numeros2);//NO ME SALIÓ
---
+++ borrador.js
@@ -0,0 +1,133 @@
+//DIFERENTES PRUEBAS
+// let dividendo1 = 3;
+// function prueba(dividendo) {
+//     for (let i = 3; i <= 5; i++) {
+//         dividendo++;
+//         console.log(dividendo);
+//     }
+// }
+// prueba(dividendo1);
+
+const e = require("express");
+
+
+// function boolean(a) {
+//         let operacion = 
+//         if ( a / a == 1) {
+//             return true;
+//         }
+// }
+// boolean(2);
+
+
+//divide and conquer para algorito de mi numeros primos.
+
+// let numeroA = 2;
+
+// if (numeroA % numeroA == 0) {
+//     let significa = "puede ser primo";
+//     console.log(significa);
+// }
+
+// if (numeroA % 1 == 0) {
+//     let significa = "puede ser primo";
+//     console.log(significa);
+// }
+
+// if (numeroA % "3 o más" == "da un residuo de 0") {
+//     let significa = "puede ser primo";
+//     console.log(significa);
+// }
+
+// function primo(n) {
+//     let divididoPorElmismo = true;
+//     let divididoPor1 = true;
+//     let divididoPorMasNumeros = true;
+//     for (let i = 2; i <= n; i++) {
+//         if (i % i == 0) {
+//             return divididoPorElmismo;
+//         }
+//     };
+//     for (let i = 2; i <= n; i++) {
+//         if (i % 1 == 0) {
+//             return divididoPor1;
+//         }
+//     };
+//     let mas = 2;
+//     for (let i = 2; i <= n; i++) {
+//         mas++;
+//         if (i % mas == 0) {
+//             return divididoPorMasNumeros;
+//         }
+//     };
+
+// }
+// primo(7);
+
+// function primo1(n1) {
+//         let divididoPorElmismo = true;
+//         let divididoPor1 = true;
+//         let divididoPorMasNumeros = true;
+//         for (let i = 2; i <= n; i++) {
+//             if (i % i == 0) {
+//                 for (let i = 2; i <= n; i++) {
+//                     if (i % 1 == 0) {
+//                         let mas = 2;
+//                         for (let i = 2; i <= n; i++) {
+//                             mas++;
+//                             if (i % mas == 0) {
+//                                 return divididoPorMasNumeros;
+//                             }
+//                         };
+//                     }
+//                 };
+//             }
+//         };
+// }
+
+// function primo3(n3) {
+//     let mas = 2;
+//     for(let i = 2; i <= n3; i++) {
+//         mas++;
+//         if (i % mas == 0) {
+//             console.log("no es primo");
+//         } else {
+//             console.log("aquí ando");
+//         }
+//     }
+// }
+// primo3(5);
+let usuarios = [
+    {id: 1, activo: true},
+    {id: 2, activo: false},
+    {id: 3, activo: false},
+];
+
+// let every = usuarios.every(u => {
+//     console.log(`Está activo ${u.id}`);
+//     return u.activo;
+// });
+
+// console.log(every);
+
+
+// let algunoActivo = usuarios.some(u => {
+//     console.log(u.id);
+//     return u.activo;
+// })
+
+// console.log(algunoActivo);
+
+
+const usuarios1 = [
+    {edad: 14, name: "Chobe"},
+    {edad: 28, name: "Andrea"},
+    {edad: 60, name: "Grace"},
+    {edad: 17, name: "Kevin"}
+];
+
+let mayores = usuarios1.filter(u => u.edad > 17);
+// console.log(mayores);
+
+const lista = usuarios1.map(u => u.name);
+console.log(lista);
