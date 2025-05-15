f3fb7232940efa3af630ce46611d2f5fdfc4ff34
LOMELINUX <kevin.lome@hotmail.com>
2025-05-15 16:13:05 -0600

    Add new algorithms

--- src/algorithms/algorithms3.js
+++ src/algorithms/algorithms3.js
@@ -103,3 +103,65 @@ function media(arr) {
     console.log(media);
 }
 // media([1, 2, 3, 4]);//CORRECTO
+
+
+
+//6.- Calcular la mediana.
+function mediana(arr) {
+    let sortArr = arr.sort();
+    if (sortArr.length % 2 == 0) {
+        let dato1 = sortArr.length / 2;
+        let dato2 = dato1 - 1;
+        let dividendo = sortArr[dato1] + sortArr[dato2];
+        let medianaPar = dividendo / 2;
+        console.log(`El número de datos es par, por lo tanto el valor de la mediana es ${medianaPar}`);
+    } else {
+        let len = sortArr.length - 1;
+        let medianaImpar = len / 2;
+        console.log(`El número de datos es impar, por lo tanto el valor de la mediana es ${sortArr[medianaImpar]}`);
+    }
+    // console.log(sortArr);
+
+}
+// mediana([1, 4, 3, 6, 7, 8, 9, 5, 9, 2]); //CORRECTO
+
+
+
+//7.-Dado 10 números enteros, calcular la suma de los números pares, cuántos numeros pares hay y la media de los números impares. 
+function enteros(arr) {
+    let pares = [];
+    let impares = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            pares.push(arr[i]);
+        } else {
+            impares.push(arr[i]);
+        }
+    }
+    let sumaImpares = impares.reduce((acc, el) => {
+        return acc + el;
+    }, 0);
+    let divisor = impares.length;
+    let mediaImpar = sumaImpares / divisor;
+    let sumaPares = pares.reduce((acc, el) => {
+        return acc + el;
+    }, 0);
+    let cuantosPares = pares.length; 
+    console.log(`El valor de la suma de los números pares es ${sumaPares}`);
+    console.log(`Existen ${cuantosPares} números pares.`);
+    console.log(`El valor de la media de los números impares es ${mediaImpar}.`);
+}
+// enteros([23, 56, 3, 5, 2, 99, 87, 45, 20, 11]); //CORRECTO
+
+
+
+//8.- Escribir la suma de los primeros 10 números pares.
+function diezPares(arr) {
+    let pares = [];
+    if (arr.length >= 10) {
+        console.log("si se puede");
+    } else {
+        console.log("Se necesitan mínimo 10 números.");
+    }
+}
+diezPares([1, 2, 3, 4, 5, 6, 7, 8, 8, 9,33, 44, 55, 66, 754, 3,2,]);
